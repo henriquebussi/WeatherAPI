18d5f0d52e0cc8d4265b392b6e99e1647b6fe908
henriquebussi <henriquebussidossantos@gmail.com>
2024-02-22 15:17:44 -0300

    api

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require ('express')
+const axios = require ('axios')
+const path = require ('path')
+const cors = require ('cors')
+const config = require ('./config.json')
+const apikey = config.apikey
+
+const app = express()
+app.listen(3000);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(payh.join(__dirname, 'public')));
+
+const traducaoClima = {
+    "few clouds": "Poucas nuvens",
+    "scattered clouds": "Nuvens dispersas"
+}
+
+app.get('/climatempo/:cidade', async(req, res) => {
+    const city = req.params.cidade
+    try{
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`)
+        if(response.status === 200){
+
+            const clima = traducaoClima[response.data.weather[0].description || response.data.weather[0].description]
+
+            const weatherData = {
+                Temperatura: response.data.main.temp,
+                Umidade: response.data.main.humidity,
+                VelocidadeDoVento: response.data.main.speed,
+                Clima: clima
+            }
+            res.send(weatherData)
+
+        } else {
+            res.send(response.status).send({erro: 'Erro ao obter dados metereologicos'})
+        }
+    } catch (erro){
+        res.status(500).send({erro:'Erro ao obter dados metereológicos'})
+    }
+})
+console.log();
